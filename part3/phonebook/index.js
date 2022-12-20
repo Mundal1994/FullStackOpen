8454da558a67cbd784d8078d9fa320ae23d5f712
Mundal1994 <manga_flip@hotmail.com>
2022-12-20 19:34:20 +0200

    finished part 3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -10,6 +11,7 @@ morgan.token('body', req => {
 app.use(express.json())
 //app.use(morgan("tiny"))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
 
 let notes = [
     { 
@@ -107,7 +109,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
